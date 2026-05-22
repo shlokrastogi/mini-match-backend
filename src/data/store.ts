480ad9a98d19d1d7e267b8a3399893d1a7e7c130
Shlok Rastogi <shlokrastogi2004@gmail.com>
2026-05-23 02:39:16 +0530

    added the hard coded data of array of objects for the candidates and the jobs

---
+++ src/data/store.ts
@@ -0,0 +1,46 @@
+export const candidates = [
+  {
+    id: "1",
+    name: "Shlok",
+    skills: ["React", "Node"],
+    yearsExperience: 2,
+    location: "Delhi",
+  },
+  {
+    id: "2",
+    name: "Aman",
+    skills: ["Python"],
+    yearsExperience: 1,
+  },
+  {
+    id: "3",
+    name: "Ravi",
+    skills: ["Java", "Spring"],
+    yearsExperience: 4,
+    location: "Mumbai",
+  },
+  {
+    id: "4",
+    name: "Neha",
+    skills: ["React"],
+    yearsExperience: 1,
+    location: "Delhi",
+  },
+];
+
+export const jobs = [
+  {
+    id: "101",
+    title: "Frontend Dev",
+    requiredSkills: ["React"],
+    minYearsExperience: 1,
+    location: "Delhi",
+  },
+  {
+    id: "102",
+    title: "Backend Dev",
+    requiredSkills: [], // No skills are required
+    minYearsExperience: 0,
+    location: "Remote",
+  },
+];
