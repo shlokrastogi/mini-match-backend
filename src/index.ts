60fe7d53201c4daf6755db9b16861f196f0fda81
Shlok Rastogi <shlokrastogi2004@gmail.com>
2026-05-23 21:51:19 +0530

    backend ready for deploy

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,18 @@ import matchRoutes from "./routes/matchRoutes";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  }),
+);
 app.use(express.json());
 
 app.use("/jobs", jobRoutes);
 app.use("/candidates", candidateRoutes);
 app.use("/match", matchRoutes);
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
